5202d3ecfbdd9b81f610bb00e831210e8b4b71fe
Akhil J Medackal <55994693+akhiljmedackal@users.noreply.github.com>
2023-11-21 12:32:23 +0530

    newProject Initial update

---
+++ Controllers/RegisterationController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegisterationController : ControllerBase
+    {
+        public async Task<IActionResult> Register ()
+        {
+            
+        }
+    }
+}
